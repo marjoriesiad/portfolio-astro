fcc44108754a5b201878f750c3b7bfb36d05ccf1
Marjorie <marjoriesiad@gmail.com>
2025-08-07 22:40:33 +0200

    chore: ajout de l'adaptateur vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import vue from '@astrojs/vue';
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
   integrations: [vue()],
   vite: {
     plugins: [tailwindcss()]
